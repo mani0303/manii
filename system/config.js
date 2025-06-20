e237a15fe5e630d4483949335e153c5ed9fa2a52
mani0303 <67160204+mani0303@users.noreply.github.com>
2025-06-20 10:53:43 +0500

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,21 @@
+/*
+┏━━━━━━━━━━━━━━━┓  
+┃ RIKZZ BASE - WHATSAPP     
+┣━━━━━━━━━━━━━━━┛
+┃♕ Creator: ryzzxd           
+┃♕ AI Helper: ChatGPT             
+┃♔ Version: 1.0.0                   
+┗━━━━━━━━━━━━━━━┛
+*/
+//========RIKZZ========
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6282189474878"] 
+global.namabot = 'SimpleCrasher V1.1'
+//======================
+global.mess = { 
+owner: 'waduhh!, lu bukan owner gw bg',
+premium: 'anda bukan user premium',
+succes: 'done bang'
+}
+//======================
